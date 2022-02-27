fde51cc9be53b1171b10889e9b758a04692078c4
Miguel <hp0826022019@unab.edu.sv>
2022-02-26 19:35:58 -0600

    Update en modelo he index

--- Lab1/Controllers/HomeController.cs
+++ Lab1/Controllers/HomeController.cs
@@ -13,11 +13,21 @@ namespace Lab1.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(ActionNotas notas)
         {
+            ViewBag.l1 = notas.Lab1;
+            ViewBag.l2 = notas.Lab2;
+            ViewBag.l3 = notas.Lab3;
+            ViewBag.p1 = notas.Par1;
+            ViewBag.p2 = notas.Par2;
+            ViewBag.p3 = notas.Par3;
+            Double promLab = ((notas.Lab1+notas.Lab2+notas.Lab3)/3)*0.40;
+            Double promPar = ((notas.Par1+notas.Par2+notas.Par3)/3)*0.60;
+            Double promFinal= promLab + promPar;
+            ViewBag.promf = promFinal;
             return View();
         }
-
+        
         public IActionResult Privacy()
         {
             return View();
---
+++ Lab1/Models/ActionNotas.cs
@@ -0,0 +1,12 @@
+namespace Lab1.Models
+{
+    public class ActionNotas
+    {
+        public double Lab1 {get; set;}
+        public double Lab2 {get; set;}
+        public double Lab3 {get; set;}
+        public double Par1 {get; set;}
+        public double Par2 {get; set;}
+        public double Par3 {get; set;}
+    }
+}
